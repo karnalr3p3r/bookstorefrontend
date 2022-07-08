e078d80fa4c62b933ffd70135ce0d6bad307a3d0
karnalr3p3r <devereauxma97@gmail.com>
2022-07-07 20:18:22 -0500

    added axios call

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      books: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:5000/book/get")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="app">
