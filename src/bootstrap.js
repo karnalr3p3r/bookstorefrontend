de8324d53310b48969505a8ac1585d17e0c161b0
karnalr3p3r <devereauxma97@gmail.com>
2022-07-08 20:55:09 -0500

    added styles

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./components/app";
 import AddBook from "./components/add-book";
+import Navbar from "./components/navbar";
 
 import "./style/main.scss";
 
 function main() {
   ReactDOM.render(
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route exact path="/">
           <App />
--- src/components/add-book.js
+++ src/components/add-book.js
@@ -15,36 +15,42 @@ export default function addBook() {
         price: price,
         description: description,
       })
-      .then((response) => console.log(response))
+      .then((response) => console.log("Book Added", response))
       .catch((error) => console.log(error));
   };
 
   return (
     <div>
-      <h1>add a book!</h1>
+      <div className="add-book">
+        <h1 className="add-book-title">Enter book data</h1>
+      </div>
       <form onSubmit={postBook}>
         <input
+          className="add-book-form-input"
           onChange={(event) => setName(event.target.value)}
           type="text"
           placeholder="name"
         />
         <input
+          className="add-book-form-input"
           onChange={(event) => setAuthor(event.target.value)}
           type="text"
           placeholder="author"
         />
         <input
+          className="add-book-form-input"
           onChange={(event) => setPrice(event.target.value)}
           type="number"
           placeholder="price"
         />
         <textarea
+          className="add-book-form-textarea"
           onChange={(event) => setDescription(event.target.value)}
           type="text"
           placeholder="description"
         />
 
-        <button>Add Book</button>
+        <button className="add-book-form-button">Add Book</button>
       </form>
     </div>
   );
