2c871e6d7d7334846f3742dd73d2cd79c79f08ba
karnalr3p3r <devereauxma97@gmail.com>
2022-07-11 21:38:44 -0500

    background image

--- src/components/book.js
+++ src/components/book.js
@@ -2,11 +2,22 @@ import React from "react";
 
 export default function Book(props) {
   return (
-    <div>
-      <h2>{props.bookData.name}</h2>
-      <h3>{props.bookData.author}</h3>
-      <p>{props.bookData.price}</p>
-      <h5>{props.bookData.description}</h5>
+    <div className="book-wrapper">
+      <div className="book-name">
+        <h2>{props.bookData.name}</h2>
+      </div>
+      <div className="book-author">
+        <h3>{props.bookData.author}</h3>
+      </div>
+      <div className="book-description">
+        <h4>{props.bookData.description}</h4>
+      </div>
+      <div className="book-price-header">
+        <p>Price</p>
+      </div>
+      <div className="book-price'">
+        <p>{props.bookData.price}</p>
+      </div>
     </div>
   );
 }
